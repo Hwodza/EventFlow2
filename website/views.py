47a13eb20fc17340ddbbc0885508187d83bec208
Hwodza <hwodza@gmail.com>
2024-07-23 19:56:07 -0400

    did stuff

--- website/views.py
+++ website/views.py
@@ -4,5 +4,15 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
-def home():
+def landing():
     return render_template("landing.html")
+
+
+@views.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@views.route('/team')
+def teams():
+    return render_template("team.html")
