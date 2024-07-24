911c6b40efcc715d554bc14e68a84130cb5c0f4c
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2024-07-24 11:17:38 -0400

    added sign up

--- website/main.py
+++ website/main.py
@@ -5,7 +5,8 @@
 
 from flask import Flask
 from flask_mysqldb import MySQL
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, session, redirect, url_for
+
 
 
 views = Blueprint('views', __name__)
@@ -22,11 +23,7 @@ db = MySQL(app)
 
 @app.route('/')
 def landing():
-    cur = db.connection.cursor()
-    cur.execute("SELECT * FROM venue")
-    fetchdata = cur.fetchall()
-    cur.close()
-    return render_template("landing.html", data=fetchdata)
+    return redirect(url_for('login'))
 
 
 @app.route('/dashboard')
@@ -38,8 +35,25 @@ def dashboard():
 def teams():
     return render_template("team.html")
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        login_data = request.form
+        email = login_data['email']
+        password = login_data['password']
+        cur = db.connection.cursor()
+        cur.execute("SELECT * FROM user WHERE case_email=%s AND password=%s", (email, password))
+        fetchdata = cur.fetchall()
+        cur.close()
+        print(fetchdata)
+
+        if len(fetchdata) == 1:
+            session['user_email'] = login_data['email']
+            flash('Logged in successfully!', category='success')
+            return redirect(url_for('dashboard'))
+        else:
+            print("Invalid credentials")
     data = request.form
     print(data)
     return render_template("login.html", )
@@ -49,19 +63,35 @@ def login():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        first_name = request.form.get('firstname')
+        last_name = request.form.get('lastname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 characters.', category='error')
+        if email[len(email)-9:] != "@case.edu":
+            flash('Email must be a case email.', category='error')
+        elif len(first_name) < 1:
+            flash('First name must be at least 1 character.', category='error')
+        elif len(last_name) < 1:
+            flash('Last name must be at least 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            cur = db.connection.cursor()
+            cur.execute("SELECT * FROM user WHERE case_email=%s", (email,))
+            fetchdata = cur.fetchall()
+            cur.close()
+            if len(fetchdata) > 0:
+                flash('Email already in use.', category='error')
+                return render_template("sign_up.html")
+            cur = db.connection.cursor()
+            cur.execute("SELECT MAX(user_id) FROM user")
+            fetchdata = cur.fetchall()
+            cur.close()
+            user_id = fetchdata[0][0] + 1
+            cur = db.connection.cursor()
+            cur.execute("INSERT INTO user (user_id, first_name, last_name, case_email, password) VALUES (%s, %s, %s, %s, %s)", (user_id, first_name, last_name, email, password1))
             flash('Account created!', category='success')
         print(email, first_name, password1, password2)
     return render_template("sign_up.html")
